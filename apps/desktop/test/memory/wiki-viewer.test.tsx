f74a79fc6a7bcc1adbca8ad34c3c85168dbf44c7
forge18 <ndurham@gmail.com>
2026-08-22 20:13:24 -0500

    test(memory): cover wiki viewer

---
+++ apps/desktop/test/memory/wiki-viewer.test.tsx
@@ -0,0 +1,10 @@
+import { render } from '@solidjs/testing-library'
+import { describe, expect, it } from 'vitest'
+import { MemoryWikiFixture } from '../../src/screens/memory/MemoryFixtures'
+
+describe('Memory wiki viewer', () => {
+  it('identifies outline links provenance and graph', () => {
+    const { getByTestId } = render(() => <MemoryWikiFixture />)
+    expect(getByTestId('v2-memory-wiki').getAttribute('data-wiki-viewer')).toBe('outline-links-provenance-graph')
+  })
+})
