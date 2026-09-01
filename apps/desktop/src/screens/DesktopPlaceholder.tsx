b1bc0f8a153e5bcdda522c4a9a86fbbd1e4ec478
forge18 <ndurham@gmail.com>
2026-09-01 15:04:44 -0500

    chore(screens): remove unreachable legacy views

--- apps/desktop/src/screens/DesktopPlaceholder.tsx
+++
@@ -1,5 +0,0 @@
-import type { View } from '../nav'
-
-export function DesktopPlaceholder(props: { view: View }) {
-  return <section class="screen-placeholder" data-testid={`screen-${props.view}`}><h1>{props.view}</h1></section>
-}
